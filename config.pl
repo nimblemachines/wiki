d061cc4916b57a93b7ab2965304d117ea602ecdf
David Frech <david@nimblemachines.com>
2005-01-27 21:53:36 +0000

    split out config.pl, so local config can be separate from wiki code.
    git-svn-id: file:///home/david/tashtego/svn/trunk/wiki@294 f1bc7e2d-46f1-0310-96be-f91141564092

---
+++ config.pl
@@ -0,0 +1,13 @@
+# This file is for local configuration variables.
+
+$webhamster = 'webhamster@nimblemachines.com';
+$wikiname = "Nimble Machines";
+$editscript = "wiki";
+
+$iconimgsrc = "icons/lambda.gif";
+$iconimgalt = "lambda the ultimate!";
+$style = "screen.css";
+$keywords = "nimble machines,wiki";
+$description = "A wiki for discussing the present and future of computing.";
+$copyright = "All content on nimblemachines.com is copyrighted. All rights are reserved.";
+
