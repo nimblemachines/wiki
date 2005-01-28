77edaa4a70cbd0966121d9c6a3411b33409a0613
David Frech <david@nimblemachines.com>
2005-01-28 02:57:19 +0000

    Changed config.pl to be "overrides" of configuration. Mostly it will have to override very few things.
    inline_markup is not called if line starts with blanks - so preserving pre.
    
    moved link creation to _before_ [[ link ]] code. maybe now interwiki links
    and camelcase will be possible inside [[ ]]. I'll have to try it...
    
    
    git-svn-id: file:///home/david/tashtego/svn/trunk/wiki@304 f1bc7e2d-46f1-0310-96be-f91141564092

--- config.pl
+++ config.pl
@@ -1,13 +1,2 @@
-# This file is for local configuration variables.
-
-$webhamster = 'webhamster@nimblemachines.com';
-$wikiname = "Nimble Machines";
-$editscript = "wiki";
-
-$iconimgsrc = "images/lambda.gif";
-$iconimgalt = "lambda the ultimate!";
-$style = "screen.css";
-$keywords = "nimble machines,wiki";
-$description = "A wiki for discussing the present and future of computing.";
-$copyright = "All content on nimblemachines.com is copyrighted. All rights are reserved.";
-
+# $Id$
+# This file is for local (site) overrides of configuration variables.
