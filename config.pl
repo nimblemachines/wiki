437b237c287e3e38cb0f405ed1cd6e586f2c444c
David Frech <david@nimblemachines.com>
2005-02-02 07:20:01 +0000

    Moved _all_ site (local) configuration - stuff that "personalizes" the wiki - into config.pl. Only the basic wiki pattern variables are still defined in the main wiki script.
    git-svn-id: file:///home/david/tashtego/svn/trunk/wiki@324 f1bc7e2d-46f1-0310-96be-f91141564092

--- config.pl
+++ config.pl
@@ -1,2 +1,25 @@
+#
 # $Id$
-# This file is for local (site) overrides of configuration variables.
+#
+# This file is for local (site) configuration of the wiki script.
+# PLEASE change these values from their defaults!
+#
+
+### Set up the config variables ###
+$webhamster = 'webhamster@example.com';
+$wikiname = "My Wiki";
+$defaultpage = "WelcomePage";
+$editscript = "wiki";
+$iconimgsrc = "mylogo";
+$iconimgalt = "my logo rocks!";
+$style = "screen";
+$keywords = "wiki";
+$description = "A wiki for discussing the things that I care about.";
+$copyright = "All content on example.com is copyrighted. All rights are reserved.";
+$heading_template = "<home />\n<h1>. <heading /></h1>";
+
+# where pages and page archive are located
+$pagedir = "pages";
+$archivedir = "$pagedir/archive";
+
+
